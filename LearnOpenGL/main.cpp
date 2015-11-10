ddc636c915ae370cc4b94cb1a5e759ad39a298e2
zilongshanren <guanghui8827@126.com>
2015-11-10 14:36:04 +0800

    Initial Commit

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by guanghui on 11/10/15.
+//  Copyright © 2015 guanghui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
